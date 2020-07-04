69c9baacc4aa81e2efec3ed21a8bde62824c0d40
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-07-04 16:09:47 +0300

    Modify routing scheme

--- ElectricShop.Tests/UnitTest1.cs
+++ ElectricShop.Tests/UnitTest1.cs
@@ -31,7 +31,7 @@ namespace ElectricShop.Tests
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
 
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null, 2).Model;
 
             Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
@@ -40,7 +40,7 @@ namespace ElectricShop.Tests
         }
 
         [TestMethod]
-        public void Can_Send_PAgination_View_Model()
+        public void Can_Send_Pagination_View_Model()
         {
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
             mock.Setup(m => m.Products).Returns(new Product[]
@@ -55,13 +55,36 @@ namespace ElectricShop.Tests
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
 
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null, 2).Model;
 
             PagingInfo pageInfo = result.PagingInfo;
             Assert.AreEqual(pageInfo.CurrentPage, 2);
             Assert.AreEqual(pageInfo.ItemsPerPage, 3);
             Assert.AreEqual(pageInfo.TotalItems, 5);
             Assert.AreEqual(pageInfo.TotalPages, 2);
+        }
+        
+        [TestMethod]
+        public void Can_Filter_Products()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1 , Name = "P1", Category = "Cat1" },
+                new Product { ProductID = 2 , Name = "P2", Category = "Cat2" },
+                new Product { ProductID = 3 , Name = "P3", Category = "Cat3" },
+                new Product { ProductID = 4 , Name = "P4", Category = "Cat2" },
+                new Product { ProductID = 5 , Name = "P5", Category = "Cat3" }
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            Product[] result = ((ProductsListViewModel)controller.List("Cat2", 1).Model).Products.ToArray();
+                                    
+            Assert.AreEqual(result.Length, 2);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
         }
     }
 }
--- ElectricShop.Web/Controllers/ProductController.cs
+++ ElectricShop.Web/Controllers/ProductController.cs
@@ -18,11 +18,12 @@ namespace ElectricShop.Web.Controllers
             repository = productRepository;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = repository.Products
+                .Where(p => category == null || p.Category == category)
                 .OrderBy(p => p.ProductID)
                 .Skip((page - 1) * PageSize)
                 .Take(PageSize),
@@ -32,7 +33,8 @@ namespace ElectricShop.Web.Controllers
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory = category
             };
             return View(model);
         }
