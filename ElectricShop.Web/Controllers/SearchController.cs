94de283b21fa614ec36a0b2cf85ac02a19bae82f
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-07-05 14:01:22 +0300

    Add search controller

---
+++ ElectricShop.Web/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using ElectricShop.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectricShop.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private IProductRepository repository;
+
+        public SearchController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+
+        public ActionResult Index(string searchString)
+        {
+            var items = repository.Products;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                items = items.Where(s => s.Name.Contains(searchString));
+            }
+
+            return View(items.ToList());
+        }
+    }
+}
