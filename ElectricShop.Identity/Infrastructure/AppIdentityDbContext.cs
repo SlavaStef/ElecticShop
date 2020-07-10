d04fdbb850d6ad5ceb4930435bc6ad4765dbfe13
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-07-11 00:35:57 +0300

    Add infrastructure, models and configuration for Identity

---
+++ ElectricShop.Identity/Infrastructure/AppIdentityDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ElectricShop.Identity.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace ElectricShop.Identity.Infrastructure
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext() : base("IdentityDb") { }
+
+        public static AppIdentityDbContext Create() => new AppIdentityDbContext();
+    }
+
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<AppIdentityDbContext>
+    {
+        protected override void Seed(AppIdentityDbContext context)
+        {
+            PerformIntialSetup(context);
+            base.Seed(context);
+        }
+
+        public void PerformIntialSetup(AppIdentityDbContext context)
+        {
+
+        }
+    }
+}
---
+++ ElectricShop.Web/App_Start/IdentityConfig.cs
@@ -0,0 +1,18 @@
+﻿using ElectricShop.Identity.Infrastructure;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Owin;
+
+namespace ElectricShop.Web.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<AppIdentityDbContext>(AppIdentityDbContext.Create);
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+
+            app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions { AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie, LoginPath = new PathString("/Account/Login") });
+        }
+    }
+}
