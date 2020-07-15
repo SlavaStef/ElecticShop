804e1d31691b6724c215bbdd4a1c32781848270d
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-07-15 23:50:54 +0300

    Add view and edit methods and views

---
+++ ElectricShop.Web/Areas/Administration/Controllers/ProductAdminController.cs
@@ -0,0 +1,28 @@
+﻿using ElectricShop.Data.Abstract;
+using ElectricShop.Data.Entities;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ElectricShop.Web.Areas.Administration.Controllers
+{
+    public class ProductAdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductAdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            return View(product);
+        }
+    }
+}
