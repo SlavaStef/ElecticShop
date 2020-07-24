37f7133ac5e6ed602d772fc9719b93367a3d3a8b
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-07-24 23:47:27 +0300

    Add Account controller and models

---
+++ ElectricShop.Common/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElectricShop.Common.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ ElectricShop.Web/Controllers/AccountController.cs
@@ -0,0 +1,91 @@
+﻿using ElectricShop.Common.DTO;
+using ElectricShop.Common.Models;
+using ElectricShop.Logic.Interfaces;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectricShop.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IUserService UserService { get { return HttpContext.GetOwinContext().GetUserManager<IUserService>(); } }
+        private IAuthenticationManager AuthenticationManager { get { return HttpContext.GetOwinContext().Authentication; } }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            await SetInitialDataAsync();
+
+            if (ModelState.IsValid)
+            {
+                UserDTO userDTO = new UserDTO { Email = model.Email, Password = model.Password };
+                ClaimsIdentity claim = await UserService.Authenticate(userDTO);
+
+                if (claim == null)
+                    throw new Exception();
+                else
+                {
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(new AuthenticationProperties { IsPersistent = true }, claim);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            await SetInitialDataAsync();
+            if (ModelState.IsValid)
+            {
+                UserDTO userDto = new UserDTO
+                {
+                    Email = model.Email,
+                    Password = model.Password,
+                    Address = model.Address,
+                    Name = model.Name,
+                    Role = "user"
+                };
+                return View("SuccessRegister");
+            }
+            return View(model);
+        }
+
+        private async Task SetInitialDataAsync()
+        {
+            await UserService.SetInitialData(new UserDTO
+            {
+                Email = "somemail@mail.ru",
+                UserName = "somemail@mail.ru",
+                Password = "ad46D_ewr3",
+                Name = "Семен Семенович Горбунков",
+                Address = "ул. Спортивная, д.30, кв.75",
+                Role = "admin",
+            }, new List<string> { "user", "admin" });
+        }
+    }
+}
