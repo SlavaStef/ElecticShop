eb70850faf540ad01051617820b30fa765e245e1
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-07-25 14:03:55 +0300

    Add edit users

--- ElectricShop.Web/Areas/Administration/Controllers/AdminUserController.cs
+++ ElectricShop.Web/Areas/Administration/Controllers/AdminUserController.cs
@@ -43,6 +43,48 @@ namespace ElectricShop.Web.Areas.Administration.Controllers
             return View("Error", new string[] { "User not found" });
         }
 
+        public async Task<ActionResult> Edit(string id)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(string id, string email, string password)
+        {
+            AppUser user = await UserManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                user.Email = email;
+                IdentityResult validEmail = await UserManager.UserValidator.ValidateAsync(user);
+
+                IdentityResult validPass = null;
+                if (password != string.Empty)
+                {
+                    validPass = await UserManager.PasswordValidator.ValidateAsync(password);
+                    if (validPass.Succeeded)
+                        user.PasswordHash = UserManager.PasswordHasher.HashPassword(password);
+                }
+
+                if ((validEmail.Succeeded && validPass == null) || (validEmail.Succeeded && password != string.Empty && validPass.Succeeded))
+                {
+                    IdentityResult result = await UserManager.UpdateAsync(user);
+
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        throw new Exception();
+                }
+            }
+
+            return View(user);
+        }
+
 
         private AppUserManager UserManager { get { return HttpContext.GetOwinContext().GetUserManager<AppUserManager>(); } } 
     }
