98904c8be9ac575bd83ce8c8acfe1890936ecd6a
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-07-28 15:36:13 +0300

    Move search to Home controller

--- ElectricShop.Web/Controllers/HomeController.cs
+++ ElectricShop.Web/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
-﻿using ElectricShop.Logic.Interfaces;
+﻿using ElectricShop.Common.DTO;
+using ElectricShop.Logic.Interfaces;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace ElectricShop.Web.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public IProductService service { get; set; }
@@ -17,6 +21,19 @@ namespace ElectricShop.Web.Controllers
         public async Task<ActionResult> Index()
         {            
             return View(await service.GetAllProductsAsync());
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Search(string searchString)
+        {
+            IEnumerable<ProductDTO> result = null;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                result = await service.FindProductsAsync(searchString);
+            }
+
+            return View(result);
         }
     }
 }
