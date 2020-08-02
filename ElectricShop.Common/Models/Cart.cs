5a2684e4372a7b5b14b92808230555323c300d56
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-08-02 19:09:07 +0300

    Add cart controller and views

---
+++ ElectricShop.Common/Models/Cart.cs
@@ -0,0 +1,37 @@
+﻿using ElectricShop.Common.DTO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElectricShop.Common.Models
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+        public IEnumerable<CartLine> Lines { get { return lineCollection; } }
+
+
+        public void AddItem(ProductDTO product, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(p => p.Product.Id == product.Id)
+                .FirstOrDefault();
+
+            if (line == null)
+                lineCollection.Add(new CartLine { Product = product, Quantity = quantity });
+            else
+                line.Quantity += quantity;
+        }
+
+        public void RemoveLine(ProductDTO product) => lineCollection.RemoveAll(l => l.Product.Id == product.Id);
+        
+        public decimal ComputeTotalValue() => lineCollection.Sum(e => e.Product.Price * e.Quantity);
+
+        public void Clear() => lineCollection.Clear();
+    }
+
+    public class CartLine
+    {
+        public ProductDTO Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ ElectricShop.Web/Controllers/CartController.cs
@@ -0,0 +1,57 @@
+﻿using ElectricShop.Common.DTO;
+using ElectricShop.Common.Models;
+using ElectricShop.Common.ViewModels;
+using ElectricShop.Logic.Interfaces;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ElectricShop.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        public IProductService productService { get; set; }
+
+        public CartController(IProductService service)
+        {
+            productService = service;
+        }
+
+
+        public ViewResult Index(string returnUrl) => View(new CartIndexViewModel { Cart = GetCart(), ReturnUrl = returnUrl });
+
+        public async Task<RedirectToRouteResult> AddToCart(int productId, string returnUrl)
+        {
+            ProductDTO product = await productService.GetProduct(productId);
+
+            if (product != null)
+                GetCart().AddItem(product, 1);
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public async Task<RedirectToRouteResult> RemoveFromCart(int productId, string returnUrl)
+        {
+            ProductDTO product = await productService.GetProduct(productId);
+
+            if (product != null)
+                GetCart().RemoveLine(product);
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public PartialViewResult Summary() => PartialView(GetCart());
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+    }
+}
